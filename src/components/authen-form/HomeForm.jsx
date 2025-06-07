9a268eb5105db5fddf6f5eeea051f4c5dcfd19c3
VietHQ767 <vietquangthcs@gmail.com>
2025-06-07 21:41:40 +0700

    nosmoking

--- src/components/authen-form/HomeForm.jsx
+++ src/components/authen-form/HomeForm.jsx
@@ -19,7 +19,7 @@ export default function HomeForm() {
       <Header className="home-header">
         <div className="home-logo">
           <img src="/images/logo1.jpg" alt="logo" />
-          NOSMOKING.COMMMMMMMMMM
+          NOSMOKING.COM
         </div>
         <div className="home-header-right">
           <Text style={{ color: '#fff' }}>Membership</Text>
